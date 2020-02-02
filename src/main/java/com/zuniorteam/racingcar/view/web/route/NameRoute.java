bbcb05b56456501de41446f0a81dc4a7fca9fba2
Podo <cglee079@gmail.com>
2020-02-02 18:12:58 +0900

    feature : web ui 구현

---
+++ src/main/java/com/zuniorteam/racingcar/view/web/route/NameRoute.java
@@ -0,0 +1,47 @@
+package com.zuniorteam.racingcar.view.web.route;
+
+import com.zuniorteam.racingcar.util.StringUtils;
+import com.zuniorteam.racingcar.view.web.util.ThymeleafCompiler;
+import org.json.JSONArray;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.*;
+
+public final class NameRoute {
+
+    private NameRoute() {}
+
+    public static class post implements Route {
+
+        private static final String PARAMETER_NAMES = "names";
+        private static final String SPLIT_TOKEN = "\\s";
+        private static final String VIEW_FILENAME = "game";
+
+        @Override
+        public Object handle(Request request, Response response) throws Exception {
+
+            final JSONArray names = getNames(request);
+
+            final Map<String, Object> model = Collections.singletonMap("names", names);
+
+            return ThymeleafCompiler.render(model, VIEW_FILENAME);
+        }
+
+        private JSONArray getNames(Request request) {
+            final String namesString = request.queryParams(PARAMETER_NAMES);
+
+            validate(namesString);
+
+            return new JSONArray(namesString.split(SPLIT_TOKEN));
+        }
+
+        private void validate(String namesString) {
+            if (StringUtils.isEmpty(namesString)) {
+                throw new IllegalArgumentException("자동차 이름이 없습니다");
+            }
+        }
+
+    }
+}
---
+++ src/main/java/com/zuniorteam/racingcar/view/web/util/ThymeleafCompiler.java
@@ -0,0 +1,15 @@
+package com.zuniorteam.racingcar.view.web.util;
+
+import spark.ModelAndView;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+import java.util.Map;
+
+public final class ThymeleafCompiler {
+
+    private static final ThymeleafTemplateEngine THYMELEAF_TEMPLATE_ENGINE = new ThymeleafTemplateEngine();
+
+    public static String render(Map<String, Object> model, String viewFilename) {
+        return THYMELEAF_TEMPLATE_ENGINE.render(new ModelAndView(model, viewFilename));
+    }
+}
